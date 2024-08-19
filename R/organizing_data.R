eba38b40745f14fa89a6f6307c97c269dd142eb2
Sean Hatch <seantjhatch@gmail.com>
2024-08-19 16:35:09 -0500

    Adding R exercise, updating names

---
+++ R/organizing_data.R
@@ -0,0 +1,24 @@
+# Organizing data in R
+
+library(tidyverse)
+library(palmerpenguins)
+
+
+penguins %>%
+  group_by(island) %>%
+  drop_na() %>% # Drops any rows with missing data from the table
+  summarize(mean_bill_length_mm = mean(bill_length_mm))
+
+penguins %>%
+  group_by(species, island) %>%
+  drop_na() %>% # Drops any rows with missing data from the table
+  summarize(max_bill_length_mm = max(bill_length_mm))
+
+penguins %>%
+  group_by(species, island) %>%
+  drop_na() %>% # Drops any rows with missing data from the table
+  summarize(max_bill_length_mm = max(bill_length_mm), mean_bill_length_mm = mean(bill_length_mm))
+
+penguins %>%
+  filter(species == "Adelie")
+# Only info on Adelie penguins
