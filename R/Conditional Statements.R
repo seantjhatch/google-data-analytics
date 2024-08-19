da26416d90f494b3f0f16e37abdf5d1cd8ce24e4
Sean Hatch <seantjhatch@gmail.com>
2024-08-19 11:34:15 -0500

    Cleaning up code

--- R/Conditional Statements.R
+++ R/Conditional Statements.R
@@ -2,31 +2,31 @@
 
 # if
 x <- 4
-if(x > 0) {
-    print("x is a positive number")
+if (x > 0) {
+  print("x is a positive number")
 }
 
 # else
 x <- 7
-if(x > 0) {
+if (x > 0) {
   print("x is a postive number")
 } else {
   print("x is either a negative number or zero")
 }
 
 x <- -7
-if(x>0) {
+if (x > 0) {
   print("x is a postive number")
 } else {
   print("x is either a negative number or zero")
 }
-  
+
 # else if
 x <- 0
 if (x > 0) {
   print("x is a postive number")
-  } else if (x == 0) {
+} else if (x == 0) {
   print("x is zero")
-  } else {
+} else {
   print("x is a negative number")
 }
