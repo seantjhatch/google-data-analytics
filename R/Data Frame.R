044cecbdc753f2766c36851e5d4f0c68a6334a2b
Sean Hatch <seantjhatch@gmail.com>
2024-08-19 10:47:24 -0500

    First R work

--- R/Data Frame.R
+++ R/Data Frame.R
@@ -1,3 +1,5 @@
 #Creating my first data frame
+z <- data.frame(x = c(1, 2, 3), y = c(1.5, 5.5, 7.5))
 
-data.frame(x = c(1, 2, 3) , y = c(1.5, 5.5, 7.5))
+#Extracting value in row 2, column 1
+z[2, 1]
