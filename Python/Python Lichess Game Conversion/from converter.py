87cd2b7d8eb09a7a7f3f47fd9c3c6f7ca4b521cd
Sean Hatch <seantjhatch@gmail.com>
2024-08-23 16:39:02 -0500

    test

--- Python/Python Lichess Game Conversion/from converter.py
+++
@@ -1,4 +0,0 @@
-from converter.pgn_data import PGNData
-
-pgn_data = PGNData("lichess_revh_2024-08-21.pgn")
-pgn_data.export()
--- Python/pandas_example.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-sales = pd.read_csv(
-    'sales_data.csv',
-    parse_dates=['Date'])
